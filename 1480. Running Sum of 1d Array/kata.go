a84bfeeb5e685e457574d64ae0bc8bee683e4053
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2021-05-30 13:44:23 +0200

    1480. Running Sum of 1d Array

---
+++ 1480. Running Sum of 1d Array/kata.go
@@ -0,0 +1,11 @@
+package kata
+
+func RunningSum(nums []int) []int {
+	sumNums := make([]int, len(nums))
+	subTotal := 0
+	for i := 0; i < len(nums); i++ {
+		subTotal += nums[i]
+		sumNums[i] = subTotal
+	}
+	return sumNums
+}
