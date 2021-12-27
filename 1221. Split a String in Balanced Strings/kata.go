75f24c06b590f0c27d7898420ae1c817ad62b0c6
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2021-12-27 11:38:53 +0100

    1221. Split a String in Balanced Strings

---
+++ 1221. Split a String in Balanced Strings/kata.go
@@ -0,0 +1,40 @@
+package _221__Split_a_String_in_Balanced_Strings
+
+func balancedStringSplit(s string) int {
+
+	max := 0
+
+	current := s[0]
+	seekCounter := 1
+	var seek byte
+	if current == 'R' {
+		seek = 'L'
+	} else {
+		seek = 'R'
+	}
+
+	for i := 1; i < len(s); i++ {
+
+		if seek == s[i] && seekCounter == 1 {
+			max++
+			i++
+			if i == len(s) {
+				break
+			}
+			current = s[i]
+			seekCounter = 1
+			if current == 'R' {
+				seek = 'L'
+			} else {
+				seek = 'R'
+			}
+
+		} else if s[i] == current {
+			seekCounter++
+		} else if s[i] == seek {
+			seekCounter--
+		}
+	}
+
+	return max
+}
