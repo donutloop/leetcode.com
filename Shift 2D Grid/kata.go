9be1f894ed3d7c6a279050a83c4e35053b7c334f
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-01-10 09:27:16 +0100

    1260. Shift 2D Grid

---
+++ Shift 2D Grid/kata.go
@@ -0,0 +1,38 @@
+package Shift_2D_Grid
+
+func shiftGrid(grid [][]int, k int) [][]int {
+
+	swap := -1001
+	for ; k >= 1; k-- {
+		for i := 0; i < len(grid); i++ {
+			for j := 0; j < len(grid[i]); j++ {
+				c := swap
+				if c != -1001 {
+					if i == (len(grid)-1) && j == (len(grid[i])-1) {
+						grid[0][0] = swap
+						swap = -1001
+					} else if i+1 < len(grid) && j == (len(grid[i])-1) {
+						oldSwap := swap
+						swap = grid[i+1][0]
+						grid[i+1][0] = oldSwap
+					} else if j < len(grid[i]) {
+						oldSwap := swap
+						swap = grid[i][j+1]
+						grid[i][j+1] = oldSwap
+					}
+				} else {
+					if i == (len(grid)-1) && j == (len(grid[i])-1) {
+						return grid
+					} else if i+1 < len(grid) && j == (len(grid[i])-1) {
+						swap = grid[i+1][0]
+						grid[i+1][0] = grid[i][j]
+					} else if j < len(grid[i]) {
+						swap = grid[i][j+1]
+						grid[i][j+1] = grid[i][j]
+					}
+				}
+			}
+		}
+	}
+	return grid
+}
