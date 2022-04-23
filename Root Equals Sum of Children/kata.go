dca249d5196eb2aca689ec06454d44d7e58dc6d9
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-04-23 10:19:28 +0200

    2236. Root Equals Sum of Children

---
+++ Root Equals Sum of Children/kata.go
@@ -0,0 +1,11 @@
+package Root_Equals_Sum_of_Children
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func checkTree(root *TreeNode) bool {
+	return root.Val == (root.Left.Val + root.Right.Val)
+}
