913965b9cfe81219af85cfb784d12a3dd0e34d3d
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-04-26 20:23:58 +0200

    1484. Group Sold Products By The Date

--- Group Sold Products By The Date/q.sql
+++ Group Sold Products By The Date/q.sql
@@ -1,17 +1,7 @@
 SELECT
     sell_date,
     COUNT(DISTINCT product) AS num_sold,
-    (
-        SELECT
-            GROUP_CONCAT(DISTINCT product
-      ORDER BY
-         product SEPARATOR ',')
-        FROM
-            Activities AS a1
-        WHERE
-                a1.sell_date = a2.sell_date
-    )
-                            AS products
+    GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products
 FROM
     Activities AS a2
 GROUP BY
