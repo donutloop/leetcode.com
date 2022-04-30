15eeccb13314300c8e1fff091f3c046d2b2c6ea6
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-04-30 11:34:34 +0200

    619. Biggest Single Number

--- Biggest Single Number/q.sql
+++ Biggest Single Number/q.sql
@@ -1,6 +1,8 @@
-SELECT (SELECT num
-        FROM   mynumbers
-        GROUP  BY num
-        HAVING Count(num) = 1
-        ORDER  BY num DESC
-           LIMIT  1) AS NUM; 
+(SELECT num
+ FROM mynumbers GROUP  BY num
+ HAVING Count(num) = 1
+ ORDER  BY num DESC
+     LIMIT 1)
+UNION
+(SELECT NULL)
+LIMIT 1
