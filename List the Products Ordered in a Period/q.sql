0e051eaae30b86f8adfe48c223c4082168e44cb7
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-04-30 08:25:57 +0200

    1327. List the Products Ordered in a Period

---
+++ List the Products Ordered in a Period/q.sql
@@ -0,0 +1,10 @@
+SELECT p.product_name,
+       Sum(o.unit) AS unit
+FROM   products AS p
+           INNER JOIN orders AS o
+                      ON o.product_id = p.product_id
+WHERE  Month(o.order_date) = 2
+GROUP  BY p.product_name,
+    Year(o.order_date),
+    Month(o.order_date)
+HAVING unit >= 100; 
