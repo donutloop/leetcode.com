be35e2328970dc95fed806fea6cde857eda70c12
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-05-01 11:47:17 +0200

    2066. Account Balance

---
+++ Account Balance/q.sql
@@ -0,0 +1,10 @@
+SELECT
+    account_id,
+    day,
+    SUM(IF(type = 'Deposit', amount, -amount)) OVER (
+    PARTITION BY account_id
+    ORDER BY
+    day
+    ) AS balance
+FROM
+    Transactions;
