3849a64446ae956bdd1ea13024eab64289452037
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-05-01 08:24:45 +0200

    1965. Employees With Missing Information v2

--- Employees With Missing Information/q.sql
+++ Employees With Missing Information/q.sql
@@ -1,16 +1,10 @@
-SELECT employee_id
-FROM (
-         (SELECT e.employee_id,
-                 name,
-                 salary
-          FROM Employees AS e
-                   LEFT JOIN Salaries AS s ON s.employee_id=e.employee_id)
-         UNION
-         (SELECT s.employee_id,
-                 name,
-                 salary
-          FROM Salaries AS s
-                   LEFT JOIN Employees AS e ON s.employee_id=e.employee_id)) AS ec
-WHERE name IS NULL
-   OR salary IS NULL
-ORDER BY employee_id;
+(SELECT e.employee_id
+ FROM Employees AS e
+          LEFT JOIN Salaries AS s ON s.employee_id=e.employee_id
+ WHERE salary IS NULL)
+UNION
+(SELECT s.employee_id
+ FROM Salaries AS s
+          LEFT JOIN Employees AS e ON s.employee_id=e.employee_id
+ WHERE name IS NULL)
+ORDER BY employee_id
