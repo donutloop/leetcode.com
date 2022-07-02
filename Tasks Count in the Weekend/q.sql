c87ee75d84955f14f91623ec7be566565d4efc52
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-07-02 13:41:08 +0200

    2298. Tasks Count in the Weekend

---
+++ Tasks Count in the Weekend/q.sql
@@ -0,0 +1,9 @@
+SELECT Sum(CASE
+               WHEN Weekday(submit_date) <= 4 THEN 1
+               ELSE 0
+    END) AS weekend_cnt,
+       Sum(CASE
+               WHEN Weekday(submit_date) > 4 THEN 1
+               ELSE 0
+           END) AS working_cnt
+FROM   tasks; 
