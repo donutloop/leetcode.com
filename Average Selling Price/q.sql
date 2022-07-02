3cec555fe28cb57e23419dc5e9d72287b85cb251
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-07-02 11:15:42 +0200

    1251. Average Selling Price V2

--- Average Selling Price/q.sql
+++ Average Selling Price/q.sql
@@ -1,16 +1,8 @@
-SELECT a.product_id,
-       Round(Sum(a.price) / Sum(a.units), 2) AS average_price
-FROM   (SELECT p.product_id,
-               p.price * us.units AS price,
-               us.units
-        FROM   unitssold AS us
-                   INNER JOIN prices AS p
-                              ON p.product_id = us.product_id
-        WHERE  us.purchase_date >= p.start_date
-          AND us.purchase_date <= p.end_date
-        GROUP  BY us.product_id,
-                  p.price,
-                  us.units,
-                  us.purchase_date
-       ) AS a
-GROUP  BY product_id 
+SELECT p.product_id,
+       ROUND(Sum(p.price * us.units) / Sum(us.units), 2) AS average_price
+FROM   unitssold AS us
+           INNER JOIN prices AS p
+                      ON p.product_id = us.product_id
+WHERE  us.purchase_date >= p.start_date
+  AND us.purchase_date <= p.end_date
+GROUP  BY us.product_id 
