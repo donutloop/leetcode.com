6a2dd661e16d3bc551899371a8130cb1f35ed627
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-07-08 20:11:36 +0200

    1265. Print Immutable Linked List in Reverse

---
+++ Print Immutable Linked List in Reverse/kata.go
@@ -0,0 +1,24 @@
+package Print_Immutable_Linked_List_in_Reverse
+
+/*   Below is the interface for ImmutableListNode, which is already defined for you.
+ *
+ *   type ImmutableListNode struct {
+ *
+ *   }
+ *
+ *   func (this *ImmutableListNode) getNext() ImmutableListNode {
+ *		// return the next node.
+ *   }
+ *
+ *   func (this *ImmutableListNode) printValue() {
+ *		// print the value of this node.
+ *   }
+ */
+
+func printLinkedListInReverse(head ImmutableListNode) {
+	n := head.getNext()
+	if n != nil {
+		printLinkedListInReverse(n)
+	}
+	head.printValue()
+}
