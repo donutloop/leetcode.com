180d838b0b9d8d34ad84651493111e27e8a9d865
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-07-08 20:25:23 +0200

    339. Nested List Weight Sum

---
+++ Nested List Weight Sum/kata.go
@@ -0,0 +1,43 @@
+package Nested_List_Weight_Sum
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ * // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (n NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (n NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (n *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (n NestedInteger) GetList() []*NestedInteger {}
+ */
+func depthSum(nestedList []*NestedInteger) int {
+	l := 1
+	sum := 0
+	f(nestedList, &sum, l)
+	return sum
+}
+
+func f(nestedList []*NestedInteger, sum *int, l int) {
+	for _, o := range nestedList {
+		if o.IsInteger() {
+			*sum += l * o.GetInteger()
+		} else {
+			f(o.GetList(), sum, l+1)
+		}
+	}
+}
