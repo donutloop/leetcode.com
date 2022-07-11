c7dca2e56d8b1cb5a250b5bcf8d23a6b898c122c
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-07-11 20:59:15 +0200

    2231. Largest Number After Digit Swaps by Parity

---
+++ Largest Number After Digit Swaps by Parity/kata.go
@@ -0,0 +1,96 @@
+package Largest_Number_After_Digit_Swaps_by_Parity
+
+import (
+	"container/heap"
+	"math"
+)
+
+func largestInteger(num int) int {
+
+	tmpNum := num
+
+	odd := &IntHeap{}
+	even := &IntHeap{}
+
+	var i int
+
+	var bitSet int
+
+	for {
+		n := num % 10
+		num = num / 10
+		if n%2 == 0 {
+			even.Push(n)
+			if bitSet&(1<<i) == 0 {
+				bitSet |= 1 << i
+			}
+		} else {
+			odd.Push(n)
+			if bitSet&(1<<i) > 0 {
+				bitSet ^= 1 << i
+			}
+		}
+
+		if num == 0 {
+			break
+		}
+
+		i++
+	}
+	heap.Init(odd)
+	heap.Init(even)
+
+	var reordedNum int
+	num = tmpNum
+	u := int(math.Pow(10, float64(i)))
+	for ; i >= 0; i-- {
+
+		if (bitSet & (1 << i)) == 0 {
+			n := heap.Pop(odd).(int)
+			reordedNum += n * u
+		} else {
+			n := heap.Pop(even).(int)
+			reordedNum += n * u
+		}
+
+		u = u / 10
+		if u == 0 {
+			u = 1
+		}
+	}
+
+	return reordedNum
+}
+
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] > h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	if h.Len() < 2 {
+		return
+	}
+
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	if n == 0 {
+		return -1
+	}
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
