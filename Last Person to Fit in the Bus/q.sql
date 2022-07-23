9b409bd332d05fef07de0f6d26708714d927e599
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2022-07-23 13:47:41 +0200

    1204. Last Person to Fit in the Bus

---
+++ Last Person to Fit in the Bus/q.sql
@@ -0,0 +1,18 @@
+WITH cte AS
+(
+   SELECT
+      person_name,
+      SUM(weight) OVER (
+   ORDER BY
+      turn ROWS UNBOUNDED PRECEDING) AS running_total
+   FROM
+      Queue
+)
+
+SELECT
+   person_name
+FROM
+   cte
+WHERE
+   running_total <= 1000 ORDER BY running_total DESC LIMIT 1;
+    
