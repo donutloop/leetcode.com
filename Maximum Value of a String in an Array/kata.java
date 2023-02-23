db110f07c64fe8967ed706c8d31867b8c3c9bff2
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2023-02-23 10:09:24 +0100

    2496. Maximum Value of a String in an Array v2

--- Maximum Value of a String in an Array/kata.java
+++ Maximum Value of a String in an Array/kata.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maximumValue(String[] strs) {
         var max = 0;
+        outerLoop:
         for (var str: strs) {
             var localMax = 0;
             var j = 0;
@@ -8,9 +9,8 @@ class Solution {
                 if (Character.isLetter(str.charAt(i))) {
                     if (max < str.length()) {
                         max = str.length();
-                        localMax = 0;
                     }
-                    break;
+                    continue outerLoop;
                 }
                 var digit = Character.getNumericValue(str.charAt(i));
                 localMax += (digit * Math.pow(10.0, (double) j));
