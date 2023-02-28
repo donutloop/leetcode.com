54649e816e84f6ec185513f3db70b537f3cd904a
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2023-02-28 11:21:23 +0100

    1637. Widest Vertical Area Between Two Points Containing No Points V2

--- Widest Vertical Area Between Two Points Containing No Points/kata.java
+++ Widest Vertical Area Between Two Points Containing No Points/kata.java
@@ -1,21 +1,19 @@
 class Solution {
     public int maxWidthOfVerticalArea(int[][] points) {
-        Integer[][] convertedPoints = new Integer[points.length][points[0].length];
+        Integer[] convertedPoints = new Integer[points.length];
         for (int i = 0; i < points.length; i++) {
-            for (int j = 0; j < points[i].length; j++) {
-                convertedPoints[i][j] = Integer.valueOf(points[i][j]);
-            }
+             convertedPoints[i] = Integer.valueOf(points[i][0]);
         }
 
-        Arrays.sort(convertedPoints, new Comparator<Integer[]>() {
-            public int compare(Integer[] p1, Integer[] p2) {
-                return p1[0].compareTo(p2[0]);
+        Arrays.sort(convertedPoints, new Comparator<Integer>() {
+            public int compare(Integer p1, Integer p2) {
+                return p1.compareTo(p2);
             }
         });
 
         var max = 0;
         for (int i = 1; i < convertedPoints.length; i++) {
-            var d = convertedPoints[i][0]-convertedPoints[i-1][0];
+            var d = convertedPoints[i]-convertedPoints[i-1];
             if (d > max) {
                 max = d;
             }
