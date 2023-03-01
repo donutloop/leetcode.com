d4284aebd94ffa7206b538d0ec06d5083f1fe4ba
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2023-03-01 18:51:12 +0100

    2367. Number of Arithmetic Triplets V2

--- Number of Arithmetic Triplets/kata.java
+++ Number of Arithmetic Triplets/kata.java
@@ -6,27 +6,14 @@ class Solution {
             set.add(num);
         }
 
-        var count = 0;
+        var tripletCount = 0;
         for (var i = 0; i < nums.length; i++) {
             var jump = nums[i]+diff;
-            if (set.contains(jump)) {
-                var isTriplet = 1;
-                do {
-                   jump = jump+diff;
-                   if (set.contains(jump)) {
-                       isTriplet++;
-                   } else {
-                       break;
-                   }
-
-                } while(isTriplet != 2);
-
-                if (isTriplet == 2) {
-                    count++;
-                }
+            if (set.contains(jump) && set.contains(jump+diff)) {
+                tripletCount++;
             }
         }
 
-        return count;
+        return tripletCount;
     }
 }
