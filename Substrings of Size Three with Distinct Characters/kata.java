7ad86a0a9b2d09a1720ea3e039f53f042386222f
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2023-03-02 17:00:36 +0100

    1876. Substrings of Size Three with Distinct Characters

---
+++ Substrings of Size Three with Distinct Characters/kata.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countGoodSubstrings(String s) {
+        var k = 3;
+        if (s.length() < k) {
+            return 0;
+        }
+
+        var characterCount = 0;
+        var wordCount = 0;
+        HashMap<Character, Integer> windowTracker = new HashMap<>();
+        for (var i = 0; i < s.length(); i++) {
+            if (windowTracker.containsKey(s.charAt(i))) {
+                var otherIndex = windowTracker.get(s.charAt(i));
+                i = otherIndex + 1;
+                windowTracker.clear();
+                characterCount = 0;
+            }
+            windowTracker.put(s.charAt(i), i);
+            characterCount++;
+            if (characterCount == k) {
+                wordCount++;
+                windowTracker.remove(s.charAt(i - (k-1)));
+                characterCount = k-1 ;
+            }
+        }
+
+        return wordCount;
+    }
+}
