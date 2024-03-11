e25809b68d9f9b6f9d44b1d6c45d5d196956bc13
Marcel Edmund Franke <marcel.edmund.franke@gmail.com>
2024-03-11 21:11:57 +0100

    2729. Check if The Number is Fascinating

---
+++ Check if The Number is Fascinating/kata.go
@@ -0,0 +1,33 @@
+package kata
+
+func isFascinating(n int) bool {
+	var stats = [9]int{}
+	for i := 1; i <= 3; i++ {
+		ok := countDigits(n*i, &stats)
+		if !ok {
+			return false
+		}
+	}
+	return assert(&stats)
+}
+
+func countDigits(n int, stats *[9]int) bool {
+	for n > 0 {
+		var digit = n % 10
+		n = n / 10
+		if digit == 0 {
+			return false
+		}
+		stats[digit-1]++
+	}
+	return true
+}
+
+func assert(stats *[9]int) bool {
+	for _, count := range stats {
+		if count == 0 || count > 1 {
+			return false
+		}
+	}
+	return true
+}
